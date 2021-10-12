f2c09e91653875a4f9dda9b3d57e3f8c41ea1edc
Djurq <Djurrebax@gmail.com>
2021-10-12 09:49:10 +0200

    opdracht 2 done

--- src/Datastructures/FirstChildNextSibling/FirstChildNextSibling.cs
+++ src/Datastructures/FirstChildNextSibling/FirstChildNextSibling.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+
 namespace AD
 {
     public partial class FirstChildNextSibling<T> : IFirstChildNextSibling<T>
@@ -11,13 +14,52 @@ namespace AD
 
         public int Size()
         {
-               throw new System.NotImplementedException();
+            return size(root);
+        }
+
+        public int size(FirstChildNextSiblingNode<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return 1 + size(node.GetFirstChild()) + size(node.GetNextSibling());
         }
 
         public void PrintPreOrder()
         {
-            throw new System.NotImplementedException();
+            print(root, 0);
         }
 
+        public void print(FirstChildNextSiblingNode<T> node, int depth)
+        {
+            if (node == null)
+            {
+                return;
+            } 
+            
+            Console.WriteLine(new string(' ', depth) + node.data);
+            
+            if (node.firstChild != null)
+            {
+                print(node.firstChild, depth + 1);
+            }
+
+            if (node.nextSibling != null)
+            {
+                print(node.nextSibling, depth);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (root == null)
+            {
+                return "NIL";
+            }
+
+            return root.ToString();
+        }
     }
 }
